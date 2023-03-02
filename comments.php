9ed20632718f676bdf81a0246f251804a944ccb9
enasmadina <enasmadina4@gmail.com>
2023-03-02 03:49:29 +0200

    ajax

---
+++ comments.php
@@ -0,0 +1,9 @@
+<?php
+ $comments = array('thanks',"thank you" ,'good');
+ if(isset($_POST['comment'])){
+  array_push($comments,$_POST['comment']);
+ }
+ foreach($comments as $com){
+    echo  $com . '<br>' ;
+ }
+?>
